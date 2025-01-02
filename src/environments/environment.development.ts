69b4499a73e88837085e98d4ea5a4b6bde24a9e4
Matt Day <mattsday@gmail.com>
2025-01-02 10:08:49 +0000

    feat: environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+import { MAPS_PUBLIC_API_KEY } from "./maps";
+
+export const environment = {
+  mapsPublic: MAPS_PUBLIC_API_KEY,
+};
---
+++ src/environments/maps.ts
@@ -0,0 +1,1 @@
+export const MAPS_PUBLIC_API_KEY = 'AIzaSyD2b8aj51Q-7hnKmKz0sPEZUwpQdCnRa14';
